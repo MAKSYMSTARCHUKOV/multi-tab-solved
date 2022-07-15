840ba97f655cee15209fe0c0aba90348201c0b93
MAKSYMSTARHUKOV <m.starchukov@firstcasino.com.ua>
2022-07-15 20:36:15 +0300

    Init

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  main 
+    p Future content is here :&#41;
+</template>
+
+<script>
+export default {
+  name: 'MainPage'
+}
+</script>
+
+<style lang="scss" scoped>
+main{
+  width: 100%;
+  p {
+    padding: 15px;
+    text-align: center;
+    font-size: 20px;
+  }
+}
+</style>
